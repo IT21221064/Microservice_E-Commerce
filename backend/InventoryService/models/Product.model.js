085c961d85d02c2a0990d3e59674f30a7052b8a6
IT21221064 <it21221064@my.sliit.lk>
2025-04-13 17:28:29 +0530

    added offer value for product model

--- backend/InventoryService/models/Product.model.js
+++ backend/InventoryService/models/Product.model.js
@@ -31,6 +31,10 @@ const productSchema = new mongoose.Schema(
       type: Number,
       required: [true, "Product stock is required"],
     },
+    offerValue: {
+      type: Number,
+      default: null,
+    },
     isActive: {
       type: Boolean,
       default: true,
